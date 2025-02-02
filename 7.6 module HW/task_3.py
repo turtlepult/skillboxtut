9dd7d3a14070fe53ff33b0a668b9b4140951f3dd
Turtlepult <ivan_ladiygin@mail.ru>
2025-02-02 09:28:22 +0300

    perhabs a working programm has been created

---
+++ 7.6 module HW/task_3.py
@@ -0,0 +1,17 @@
+count_satisfactory = 0
+count_good = 0
+count_perfect = 0
+for count_student in range(int(input("Сколько в классе учеников? "))):
+    temp = int(input("Введите оценку ученика: "))
+    if temp == 3:
+        count_satisfactory += 1
+    elif temp == 4:
+        count_good += 1
+    else:
+        count_perfect += 1
+
+if count_satisfactory > count_good and count_satisfactory > count_perfect:
+    print("Сегодня больше троешников!")
+elif count_good > count_satisfactory and count_good > count_perfect:
+    print("Сегодня больше хорошистов!")
+else: print("Сегодня больше отличников! ")
