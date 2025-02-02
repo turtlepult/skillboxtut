51be136cc4925e58b653b7e8ae03fec360610c7f
Turtlepult <ivan_ladiygin@mail.ru>
2025-02-02 09:14:53 +0300

    perhabs a working programm has been created

---
+++ 7.6 module HW/task_1.py
@@ -0,0 +1,9 @@
+count = 0
+for correct_number in range(10):
+    check_number = int(input("Введите число: "))
+    if check_number % 2 ==0:
+        if check_number > 0:
+            count += 1
+        else: continue
+    else: continue
+print(f"Количество корректных номеров (чётных и положительных): {count}")
