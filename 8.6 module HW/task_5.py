1df44da7fed7111bd89693795c0c801fec348bd4
Turtlepult <ivan_ladiygin@mail.ru>
2025-02-02 19:12:35 +0300

    perhabs a working programm has been created

--- 8.6 module HW/task_5.py
+++ 8.6 module HW/task_5.py
@@ -1,9 +1,9 @@
-#Задача 5. Стипендия
+# Задача 5. Стипендия
 
 educational_grant = int(input("Введите ежемесячную стипендию: "))
 expenses = int(input("Введите ежемесячные расходы: "))
 extra_limit = 0
-for mounth in range(1,11):
+for mounth in range(1, 11):
     extra_limit += expenses - educational_grant
     print(f"{mounth}-й месяц: траты {expenses} рублей, не хватает {expenses - educational_grant} рублей.")
     percent = expenses / 100 * 3
