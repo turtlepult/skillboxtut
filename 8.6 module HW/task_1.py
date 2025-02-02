1a0c99a793f3dcfd15d275009984be80e1833df9
Turtlepult <ivan_ladiygin@mail.ru>
2025-02-02 17:35:19 +0300

    perhabs a working programm has been created

---
+++ 8.6 module HW/task_1.py
@@ -0,0 +1,10 @@
+#Задача 1. Космическая еда
+print("Информация о запасах гречки:")
+count = 0
+for food_supply in range(100,-1,-4):
+    if food_supply > 0:
+        print(f"Через {count} месяц: {food_supply} кг гречки в запасе")
+    elif food_supply == 0:
+        print(f"Через {count} месяц: {food_supply} кг гречки в запасе")
+        print("Запасы гречки закончились.")
+    count += 1
--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
+lens = [1,2,3,4,45645,86789,423,6723,234236]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for count, values in enumerate(lens):
+    print(count, values)
