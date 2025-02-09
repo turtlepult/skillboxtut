8d11980ac503e2e7fb051684d9d75e8f1959648b
Turtlepult <ivan_ladiygin@mail.ru>
2025-02-09 08:33:06 +0300

    perhabs a working programm has been created

---
+++ 9.6 module HW/task_2.py
@@ -0,0 +1,10 @@
+#Задача 2. Театр
+
+row = int(input("Введите количество рядов: "))
+numberOfSeats = int(input("Введите количество сидений в ряде: "))
+countMeters = int(input("Введите количество метров между рядами: "))
+
+print("\n Сцена \n")
+
+for i in range(row):
+    print("="*numberOfSeats, "*"*countMeters, "="*numberOfSeats,)
