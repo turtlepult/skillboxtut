4207eae17e3c50081e8d7acc3fb9b079768292d0
Turtlepult <ivan_ladiygin@mail.ru>
2025-02-09 08:24:07 +0300

    perhabs a working programm has been created

---
+++ 9.6 module HW/task_1.py
@@ -0,0 +1,7 @@
+#Задача 1. «Я стал новым пиратом!»
+count = 0
+for scream in range(10):
+    if input("Введите приветсвенный крик: ").lower() == "карамба":
+        count +=1
+    continue
+print(f"Новых пиратов {count}")
